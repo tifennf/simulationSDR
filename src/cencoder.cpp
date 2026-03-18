805f7e69a76bdb5fdd20e407545bdfea29a4febd
Alexander Bakalov <alexander.bakalov@etu.sorbonne-universite.fr>
2026-03-18 17:44:01 +0100

    Erreur au make

---
+++ src/cencoder.cpp
@@ -0,0 +1,19 @@
+namespace simulationSDR {
+
+#include"cencoder.h"
+
+using namespace std;
+
+void source_generate(uint8_t *U_K, size_t K) {
+
+}
+
+void codec_repetition_encode(const uint8_t *U_K, uint8_t *C_N, size_t K, size_t n_reps) {
+
+}
+
+void modem_BPSK_modulate(const uint8_t *C_N, int32_t *X_N, size_t N){
+
+}
+
+} // simulationSDR
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,25 @@
+#include<cstdlib>
+#include<cstddef>
+
+#include"cencoder.h"
+
+using namespace std;
+using namespace simulationSDR;
+
 int main(){
 
-    return 0;
+  uint8_t U_K = 0;
+  uint8_t C_N = 1;
+  
+  int32_t X_N = 2;
+
+  size_t K = 3;
+  size_t n_reps = 3;
+  size_t N = 3;
+  
+  source_generate(&U_K, K);
+  codec_repetition_encode(&U_K, &C_N, K, n_reps);
+  modem_BPSK_modulate(&C_N, &X_N, N);
+
+  return 0;
 }
