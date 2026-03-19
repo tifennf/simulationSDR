e9957a3630a25c40df48e92d4a65266a4362fe77
Alexander Bakalov <alexander.bakalov@etu.sorbonne-universite.fr>
2026-03-19 23:16:04 +0100

    Affichage premiere tentative: implémentation échoue

---
+++ include/transmitter.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+
+namespace simulationSDR {
+
+    // write into the buffer U_K  (random à chaque bit)
+    void source_generate(uint8_t *U_K, size_t K);
+
+    // read from the buffer U_K and write into the buffer C_N
+    void codec_repetition_encode(const uint8_t *U_K, uint8_t *C_N, size_t K, size_t n_reps);
+
+    // read from C_N, write into X_N
+    void modem_BPSK_modulate(const uint8_t *C_N, int32_t *X_N, size_t N);
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,40 @@
-#include<cstdlib>
-#include<cstddef>
+#include"transmitter.h"
 
-#include"cencoder.h"
+#include <iostream>
 
 using namespace std;
 using namespace simulationSDR;
 
+#define K 4 
+#define N 12
+
 int main(){
 
-  uint8_t U_K = 0;
-  uint8_t C_N = 1;
-  
-  int32_t X_N = 2;
+  uint8_t u_k[K];
+  uint8_t c_n[N];
 
-  size_t K = 3;
-  size_t n_reps = 3;
-  size_t N = 3;
+  size_t n_reps = N/K;
+
+  int32_t x_n[N];
   
-  source_generate(&U_K, K);
-  codec_repetition_encode(&U_K, &C_N, K, n_reps);
-  modem_BPSK_modulate(&C_N, &X_N, N);
+  source_generate(u_k, K);
+  codec_repetition_encode(u_k, c_n, K, n_reps);
+  modem_BPSK_modulate(c_n, x_n, N);
+
+  cout << "u_k: ";
+  for(auto i : u_k)
+    cout << static_cast<int>(i) << " ";
+  cout << endl;
+
+  cout << "c_n: ";
+  for(auto i : c_n)
+    cout << static_cast<int>(i) << " ";
+  cout << endl;
+
+  cout << "x_n: ";
+  for(auto i : x_n)
+    cout << i << " ";
+  cout << endl;
 
   return 0;
 }
