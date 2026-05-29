8e26ce8c00a0d8509e261b507f54743b28f3304d
tifennf <tifenn.fl@gmail.com>
2026-05-29 22:26:43 +0200

    test correction

--- include/simulationSDR/box_muller.h
+++ include/simulationSDR/box_muller.h
@@ -7,6 +7,7 @@
 #include <cstddef>
 
 namespace simulationSDR{
+    constexpr float TWO_PI = 6.283185307179586f;
 
     void box_muller_neon(const float* u1_array, const float* u2_array,
                          float* z0_array, float* z1_array, size_t N);
